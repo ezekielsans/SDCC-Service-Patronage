b10132b89f0c4b1a057e9a12e585277b188d4cca
Ezekiel Santos <128892368+ezekielsans@users.noreply.github.com>
2023-06-02 17:12:00 +0800

    Update ServicePatronageData.java

--- ServicePatronageData.java
+++ ServicePatronageData.java
@@ -12,6 +12,7 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import model.MyServices;
 import model.ServicePatronageView;
 
 /**
@@ -33,14 +34,20 @@ public class ServicePatronageData implements Serializable {
      */
     private CustomEntityManagerFactory customEntityManagerFactory;
 
-    private List<ServicePatronageAutoCompleteForm> servicePatronageForm;
-    private ServicePatronageAutoCompleteForm servicePatronageCompleteForm;
-    private ServicePatronageView servicePatronageView;
+    private List<ServicePatronageView> servicePatronageView;
+    private List<MyServices> myServices;
+    private List<Object[]> servicesType;
     private Integer index;
     private String scAcctno;
     private String lastName;
     private String firstName;
+    private String addNewGroup;
+    private String orgName;
     private Date birthdate;
+    private Date serviceDropdownDate;
+    private Integer type;
+    private Integer dropDown;
+
 
     /*
      * getter setter
@@ -48,32 +55,16 @@ public class ServicePatronageData implements Serializable {
     public CustomEntityManagerFactory getCustomEntityManagerFactory() {
         return customEntityManagerFactory == null ? customEntityManagerFactory = new CustomEntityManagerFactory() : customEntityManagerFactory;
     }
+//
+//    public void setCustomEntityManagerFactory(CustomEntityManagerFactory customEntityManagerFactory) {
+//        this.customEntityManagerFactory = customEntityManagerFactory;
+//    }
 
-    public void setCustomEntityManagerFactory(CustomEntityManagerFactory customEntityManagerFactory) {
-        this.customEntityManagerFactory = customEntityManagerFactory;
+    public List<ServicePatronageView> getServicePatronageView() {
+        return servicePatronageView == null ? servicePatronageView = new ArrayList<>() : servicePatronageView;
     }
 
-    public List<ServicePatronageAutoCompleteForm> getServicePatronageForm() {
-        return servicePatronageForm == null ? servicePatronageForm = new ArrayList<>() : servicePatronageForm;
-    }
-
-    public void setServicePatronageForm(List<ServicePatronageAutoCompleteForm> servicePatronageForm) {
-        this.servicePatronageForm = servicePatronageForm;
-    }
-
-    public ServicePatronageAutoCompleteForm getServicePatronageCompleteForm() {
-        return servicePatronageCompleteForm;
-    }
-
-    public void setServicePatronageCompleteForm(ServicePatronageAutoCompleteForm servicePatronageCompleteForm) {
-        this.servicePatronageCompleteForm = servicePatronageCompleteForm;
-    }
-
-    public ServicePatronageView getServicePatronageView() {
-        return servicePatronageView;
-    }
-
-    public void setServicePatronageView(ServicePatronageView servicePatronageView) {
+    public void setServicePatronageView(List<ServicePatronageView> servicePatronageView) {
         this.servicePatronageView = servicePatronageView;
     }
 
@@ -117,9 +108,69 @@ public class ServicePatronageData implements Serializable {
         this.birthdate = birthdate;
     }
 
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public List<MyServices> getMyServices() {
+        return myServices == null ? myServices = new ArrayList<>() : myServices;
+    }
+
+    public void setMyServices(List<MyServices> myServices) {
+        this.myServices = myServices;
+    }
+
+    public Integer getDropDown() {
+        return dropDown;
+    }
+
+    public void setDropDown(Integer dropDown) {
+        this.dropDown = dropDown;
+    }
+
+    public List<Object[]> getServicesType() {
+        return servicesType == null ? servicesType = new ArrayList<>() : servicesType;
+    }
+
+    public void setServicesType(List<Object[]> servicesType) {
+        this.servicesType = servicesType;
+    }
+
+    public Date getServiceDropdownDate() {
+        return serviceDropdownDate;
+    }
+
+    public void setServiceDropdownDate(Date serviceDropdownDate) {
+        this.serviceDropdownDate = serviceDropdownDate;
+    }
+
+    public String getAddNewGroup() {
+        return addNewGroup;
+    }
+
+    public void setAddNewGroup(String addNewGroup) {
+        this.addNewGroup = addNewGroup;
+    }
     /*
      * methods
      */
+
+    public void tabConvert() {
+
+    }
+
     public void init() {
         if (FacesContext.getCurrentInstance().isPostback() == false) {
             beanclear();
@@ -127,16 +178,21 @@ public class ServicePatronageData implements Serializable {
     }
 
     public void beanclear() {
-        setServicePatronageCompleteForm(null);
-        setServicePatronageForm(null);
+        setServicePatronageView(null);
         setIndex(0);
+        setLastName(null);
+        setFirstName(null);
+        setScAcctno(null);
+        setBirthdate(null);
 
     }
 
     public void tabCompleteServicePatronageDb() {
         String query;
 
-        setServicePatronageForm(null);
+        setServicePatronageView(null);
+
+        System.out.println("WAWAW");
 
         if (getScAcctno() != null
                 || getLastName() != null
@@ -161,11 +217,10 @@ public class ServicePatronageData implements Serializable {
                 }
 
                 query += "ORDER BY x.scAcctno";
+                setServicePatronageView(getCustomEntityManagerFactory().getLportalMemOrgEntityManagerFactory().createEntityManager().createQuery(query).getResultList());
+            } catch (Exception e) {
 
-                setServicePatronageForm(getCustomEntityManagerFactory().getLportalMemOrgEntityManagerFactory().createEntityManager().createQuery(query).getResultList());
-            } catch (Exception e){
-               
-                System.out.println("TitenginangYAan" + e);
+                System.out.println("ServicePatronageData" + e);
                 setLastName(null);
                 setScAcctno(null);
                 setFirstName(null);
@@ -174,5 +229,12 @@ public class ServicePatronageData implements Serializable {
             setIndex(0);
 
         }
+    }
+
+    public void select() {
+
+        setMyServices(getCustomEntityManagerFactory().getLportalMemOrgEntityManagerFactory().createEntityManager()
+                .createQuery("SELECT x FROM MyServices x ORDER BY x.serviceId").getResultList());
+        System.out.println("Working");
     }
 }
